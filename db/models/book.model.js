2af353080022e14216d5dbf0ce2314c05e806895
Eduardo Leon <eduardo@weareatomic.com>
2022-08-08 21:04:24 -0500

    connections and book schema

---
+++ db/models/book.model.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose'
+
+const BookModel = new mongoose.model('Book', new mongoose.Schema({
+    _id: Number,
+    title: {
+        type: String,
+        required: true,
+        minlength1: 1,
+        maxlength: 2000,
+        trim: true
+    },
+    author: {
+        type: String,
+        required: true,
+        minlength1: 1,
+        maxlength: 1000,
+        trim: true
+    },
+    dateRelease : {
+        type : Date,
+        required: true,
+    }
+}));
+
+export { BookModel };
